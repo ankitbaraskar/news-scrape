79b6ce5d015ed70027f0393570e9a0333ae8d977
Kit <46611409+ankitbaraskar@users.noreply.github.com>
2019-06-17 15:13:20 -0400

    add /scrape route

--- routes/controller.js
+++ routes/controller.js
@@ -11,6 +11,58 @@ router.get("/", function (req, res) {
     res.render("index");
 });
 
+router.get("/scrape", function (req, res) {
+    
+    axios.get("http://www.echojs.com/").then(function (response) {        
+        var $ = cheerio.load(response.data);
+        
+        $("article").each(function (i, element) {            
+            var result = {};
+            
+            result.title = $(this)
+                .children("h2")
+                .children("a")
+                .text();
+            result.summary = $(this)
+                .children("p")
+                .children("span.upvotes")
+                .text() 
+                + " up and " +
+                $(this)
+                .children("p")
+                .children("span.downvotes")
+                .text()
+                + "down, posted by "+
+                $(this)
+                .children("p")
+                .children("username")
+                .children("a")
+                .text()
+                + $(this)
+                .children("p")
+                .children("username")
+                .contents()
+                .filter(function() {
+                    return this.nodeType === 3;
+                  })
+                .text()
+            result.link = $(this)
+                .children("a")
+                .attr("href");
+
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+        });
+
+        // Send a message to the client
+        res.send("Scrape Complete");
+    });
+});
 
 
 module.exports = router;
