e4a6c2a3ae20b1be470e0fdfd1176868ff88c6f6
Kit <46611409+ankitbaraskar@users.noreply.github.com>
2019-06-17 22:39:04 -0400

    added heroku

--- public/js/app.js
+++ public/js/app.js
@@ -2,13 +2,13 @@ $(document).ready(function () {
 
     $("#button-id").on("click", function (event) {
 
-        console.log("entered jquery thing");
+        // console.log("entered jquery thing");
         $.get("/scrape")
             .then(function (data) {
                 // $("#article-div").append(`<p>${data.title}</p>`)
                 // console.log(data);
                 for (var i = 0; i < data.length; i++) {
-                    // Display the apropos information on the page
+                    
                     $("#article-id")
                         .append("<p data-id='"
                             + data[i]._id + "'>"
