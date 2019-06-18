cfe51d407b79023a032092de619fb0a6e89c77bd
Kit <46611409+ankitbaraskar@users.noreply.github.com>
2019-06-17 21:35:30 -0400

    client receiving response after hitting scrape button

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,11 @@
 $( document ).ready(function() {
    
-    $("button-id").on("click", function(){
-
-        $.get( "/srape")
+    $("#button-id").on("click", function(event){
+        
+        console.log("entered jquery thing");
+        $.get( "/scrape")
         .then(function(data){
+            // $("#article-div").append(`<p>${data.title}</p>`)
             console.log(data);
         })
         .catch(function(error){
--- routes/controller.js
+++ routes/controller.js
@@ -51,18 +51,30 @@ router.get("/scrape", function (req, res) {
                 .children("a")
                 .attr("href");
 
-            db.Article.create(result)
-                .then(function (dbArticle) {
-                    // console.log(dbArticle);
-                    console.log("sucessfully scraped and added to DB")
-                })
-                .catch(function (err) {
-                    console.log(err);
+            db.Article.deleteMany({})
+                .then(function () {
+                    db.Article.create(result)
+                        .then(function (dbArticle) {
+                            // console.log(dbArticle);
+                            console.log("sucessfully scraped and added to DB")
+                            db.Article.find({})
+                                .then(function (dbArticle) {
+                                    // If we were able to successfully find Articles, send them back to the client
+                                    res.json(dbArticle);
+                                })
+                                .catch(function (err) {
+                                    // If an error occurred, send it to the client
+                                    res.json(err);
+                                });
+                        })
+                        .catch(function (err) {
+                            console.log(err);
+                        });
                 });
         });
 
         // Send a message to the client
-        res.send("Scrape Complete");
+        // res.send("Scrape Complete");
     });
 });
 
