2c7eae8b0f6573e5a55b08bd7cfb3634f7580c3b
Kit <46611409+ankitbaraskar@users.noreply.github.com>
2019-06-17 22:09:36 -0400

    display scraped articles on page

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,25 @@
-$( document ).ready(function() {
-   
-    $("#button-id").on("click", function(event){
-        
+$(document).ready(function () {
+
+    $("#button-id").on("click", function (event) {
+
         console.log("entered jquery thing");
-        $.get( "/scrape")
-        .then(function(data){
-            // $("#article-div").append(`<p>${data.title}</p>`)
-            console.log(data);
-        })
-        .catch(function(error){
-            console.error(error);
-        });
+        $.get("/scrape")
+            .then(function (data) {
+                // $("#article-div").append(`<p>${data.title}</p>`)
+                // console.log(data);
+                for (var i = 0; i < data.length; i++) {
+                    // Display the apropos information on the page
+                    $("#article-id")
+                        .append("<p data-id='"
+                            + data[i]._id + "'>"
+                            + data[i].title + "<br />"
+                            + data[i].summary + "<br />"
+                            + data[i].link + "</p>");
+                }
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
     })
 
 });
